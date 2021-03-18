03e073aeb26c3d358b9e0753b7914ef188db8c35
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-03-18 15:30:25 +0100

    Password Generator

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var nome = prompt ( 'inserisci nome:' )
+var cognome = prompt ( 'inserisci cognome:' )
+var colore = prompt ( 'inserisci il tuo colore preferito:')
+
+document.getElementById('password').innerHTML =  nome + cognome + colore + '21';
